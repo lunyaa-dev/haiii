0c51ba4136a3cfc70b154dd4a7af2524f2b52350
Lunyaa <77119458+lunyaa-dev@users.noreply.github.com>
2025-02-11 22:25:57 +0100

    haiii :3

---
+++ example/src/index.ts
@@ -0,0 +1,4 @@
+import { haiii } from 'haiii'
+
+console.log(haiii())
+console.log(haiii('Lunyaa'))
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export function haiii(name?: string): string {
+  if (!name) {
+    return 'Haiii :3';
+  }
+  return `Haiii ${name} :3`;
+}
